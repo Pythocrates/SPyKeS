3a3d098d726ec1dd899fbe55a8e1a9a530a9a4e8
Pythocrates <23015037+Pythocrates@users.noreply.github.com>
2020-08-03 23:22:39 +0200

    Move parsing to its own module.

--- spykes/spk.py
+++ spykes/spk.py
@@ -3,45 +3,16 @@
 ''' SPyKeS - your Simple Python Key Storage.
 '''
 
-from argparse import ArgumentParser
 from pathlib import Path
 
+from spykes.parsing import _parse_args
 from spykes.store_manager import StoreManager
-
-
-def _parse_args():
-    parser = ArgumentParser()
-
-    subparsers = parser.add_subparsers(dest='subparser_name')
-    subparsers.add_parser('edit')
-    subparsers.add_parser('show')
-    aup = subparsers.add_parser('add-user')
-    aup.add_argument(
-        'public_key', type=Path, metavar='public-key',
-        help='the public key for the new user',
-    )
-    subparsers.add_parser('list-users')
-
-    asp = subparsers.add_parser('add-store')
-    asp.add_argument('name')
-    asp.add_argument('url')
-    asp.add_argument(
-        '--initial-key', '-i', required=False, type=Path,
-        help='initialize using given public key and empty keys file',
-    )
-
-    ssp = subparsers.add_parser('select-store')
-    ssp.set_defaults(name='select-store')
-    ssp.add_argument('name')
-
-    subparsers.add_parser('list-stores')
-
-    return parser.parse_args()
 
 
 def main():
     args = _parse_args()
     manager = StoreManager(Path.home() / '.config' / 'spykes')
+
     if args.subparser_name in ['add-store', 'select-store', 'list-stores']:
         func = getattr(manager, args.subparser_name.replace('-', '_'))
     elif args.subparser_name in ['edit', 'show', 'add-user', 'list-users']:
